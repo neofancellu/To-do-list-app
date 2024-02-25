b10dc42b3a714a0b7500c31571e52a5f8f581025
neofancellu <neo.fancellu@gmail.com>
2024-02-25 17:57:20 +0000

    Add project files.

---
+++ To-do list app/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace To_do_list_app
+{
+    partial class ToDoList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.descTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.newButton = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.toDoListView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.toDoListView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Candara", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LightGray;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1753, 170);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "To-do List";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.BackColor = System.Drawing.Color.LightGray;
+            this.titleTextBox.ForeColor = System.Drawing.Color.Black;
+            this.titleTextBox.Location = new System.Drawing.Point(12, 376);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(1753, 26);
+            this.titleTextBox.TabIndex = 1;
+            // 
+            // descTextBox
+            // 
+            this.descTextBox.BackColor = System.Drawing.Color.LightGray;
+            this.descTextBox.ForeColor = System.Drawing.Color.Black;
+            this.descTextBox.Location = new System.Drawing.Point(12, 599);
+            this.descTextBox.Name = "descTextBox";
+            this.descTextBox.Size = new System.Drawing.Size(1753, 26);
+            this.descTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("Candara", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.LightGray;
+            this.label2.Location = new System.Drawing.Point(12, 460);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(2681, 89);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Description:";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Font = new System.Drawing.Font("Candara", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.LightGray;
+            this.label3.Location = new System.Drawing.Point(12, 262);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(2681, 89);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Title:";
+            // 
+            // newButton
+            // 
+            this.newButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.newButton.Font = new System.Drawing.Font("Candara", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.newButton.Location = new System.Drawing.Point(12, 657);
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(420, 55);
+            this.newButton.TabIndex = 5;
+            this.newButton.Text = "New";
+            this.newButton.UseVisualStyleBackColor = false;
+            this.newButton.Click += new System.EventHandler(this.newButton_Click);
+            // 
+            // editButton
+            // 
+            this.editButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.editButton.Font = new System.Drawing.Font("Candara", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.editButton.Location = new System.Drawing.Point(438, 657);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(420, 55);
+            this.editButton.TabIndex = 6;
+            this.editButton.Text = "Edit";
+            this.editButton.UseVisualStyleBackColor = false;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.saveButton.Font = new System.Drawing.Font("Candara", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.saveButton.Location = new System.Drawing.Point(919, 657);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(420, 55);
+            this.saveButton.TabIndex = 7;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = false;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.deleteButton.Font = new System.Drawing.Font("Candara", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.deleteButton.Location = new System.Drawing.Point(1345, 657);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(420, 55);
+            this.deleteButton.TabIndex = 8;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = false;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // toDoListView
+            // 
+            this.toDoListView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.toDoListView.BackgroundColor = System.Drawing.Color.Black;
+            this.toDoListView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.toDoListView.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.toDoListView.Location = new System.Drawing.Point(12, 735);
+            this.toDoListView.Name = "toDoListView";
+            this.toDoListView.RowHeadersWidth = 62;
+            this.toDoListView.RowTemplate.Height = 28;
+            this.toDoListView.Size = new System.Drawing.Size(1740, 589);
+            this.toDoListView.TabIndex = 9;
+            // 
+            // ToDoList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1777, 1336);
+            this.Controls.Add(this.toDoListView);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.newButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.descTextBox);
+            this.Controls.Add(this.titleTextBox);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Gray;
+            this.Name = "ToDoList";
+            this.Text = "To-do List";
+            this.Load += new System.EventHandler(this.ToDoList_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.toDoListView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.TextBox descTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button newButton;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.DataGridView toDoListView;
+    }
+}
+
---
+++ To-do list app/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace To_do_list_app
+{
+    public partial class ToDoList : Form
+    {
+        public ToDoList()
+        {
+            InitializeComponent();
+        }
+
+        string connectionString = @"Data Source =D:\Programming\databases\to-doList.db;Version = 3;";
+
+        //testing if project talks to database
+        public void TestDatabaseConnection()
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Successfully connected to SQLite database!");
+                    using (var command = new SQLiteCommand("SELECT name FROM sqlite_master WHERE type='table' AND name='ToDoItems'", connection))
+                    {
+                        using (var reader = command.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                Console.WriteLine("ToDoItems table exists!");
+
+                                string query = "SELECT * FROM ToDoItems";
+                                using (var adapter = new SQLiteDataAdapter(query, connection))
+                                {
+                                    DataTable dataTable = new DataTable();
+                                    adapter.Fill(dataTable);
+
+                                    // Print data from each row
+                                    Console.WriteLine("\nTo-Do Items:");
+                                    foreach (DataRow row in dataTable.Rows)
+                                    {
+                                        Console.WriteLine("ID: {0}, Title: {1}, Description: {2}",
+                                            row["id"], row["Title"], row["Description"]);
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("ToDoItems table does not exist!");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error connecting to database: " + ex.Message);
+            }
+        }
+
+
+
+        DataTable toDoList = new DataTable();
+        bool isEditing = false;
+
+        /*
+        private void ToDoList_Load(object sender, EventArgs e)
+        {
+            
+
+            //Colums are made
+            toDoList.Columns.Add("Title");
+            toDoList.Columns.Add("Description");
+
+            //Link datasource to datagridview
+            toDoListView.DataSource = toDoList;
+        }
+        */
+
+        private void ToDoList_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // Connect to database
+                using (var connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Get table schema (column names)
+                    string query = "SELECT id, Title, Description FROM ToDoItems";
+                    using (var adapter = new SQLiteDataAdapter(query, connection))
+                    {
+                        // Fill dataTable with schema
+                        toDoList = new DataTable();
+                        toDoList.Clear();
+                        
+
+                        // Check if there are actually rows retrieved (handle empty table)
+                        if (toDoList.Rows.Count > 0)
+                        {
+                            // Remove the dummy row only if data was retrieved
+                            toDoList.Rows.RemoveAt(0);
+                        }
+                    }
+
+                    // Now populate data
+                    query = "SELECT * FROM ToDoItems"; // Fetch all rows this time
+                    using (var adapter = new SQLiteDataAdapter(query, connection))
+                    {
+                        adapter.Fill(toDoList);
+                    }
+
+                    // Link data table to the grid view
+                    
+                    toDoListView.DataSource = toDoList;
+                    toDoListView.Columns["id"].Visible = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error loading data: " + ex.Message);
+            }
+        }
+
+
+        private void newButton_Click(object sender, EventArgs e)
+        {
+            titleTextBox.Text = "";
+            descTextBox.Text = "";
+
+            TestDatabaseConnection();
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+            isEditing = true;
+            //Fill text fields with data from the Table
+            titleTextBox.Text = toDoList.Rows[toDoListView.CurrentCell.RowIndex].ItemArray[1].ToString();
+            descTextBox.Text = toDoList.Rows[toDoListView.CurrentCell.RowIndex].ItemArray[2].ToString();
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get the data table row and ID of the selected item
+                DataRow selectedRow = toDoList.Rows[toDoListView.CurrentCell.RowIndex];
+                long id = (long)selectedRow["id"];
+
+                // Check if a row is actually selected
+                if (selectedRow != null)
+                {
+                    // Delete the entry from the database
+                    using (var connection = new SQLiteConnection(connectionString))
+                    {
+                        connection.Open();
+
+                        string deleteQuery = "DELETE FROM ToDoItems WHERE id=@id";
+                        using (var command = new SQLiteCommand(deleteQuery, connection))
+                        {
+                            command.Parameters.AddWithValue("@id", id);
+                            command.ExecuteNonQuery();
+                        }
+                    }
+
+                    // Remove the row from the data table
+                    selectedRow.Delete();
+                    toDoList.AcceptChanges();
+
+                    // Update data grid view
+                    toDoListView.Refresh();
+                }
+                else
+                {
+                    MessageBox.Show("Please select a row to delete.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error deleting data: " + ex.Message);
+                // Add user-friendly error notification in your application
+            }
+        }
+
+
+        /*
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (isEditing)
+            {
+                toDoList.Rows[toDoListView.CurrentCell.RowIndex]["Title"] = titleTextBox.Text;
+                toDoList.Rows[toDoListView.CurrentCell.RowIndex]["Description"] = descTextBox.Text;
+            }
+            else
+            {
+                toDoList.Rows.Add(titleTextBox.Text, descTextBox.Text);
+            }
+
+            //Clear temp field after change
+            titleTextBox.Text = "";
+            descTextBox.Text = "";
+            isEditing = false;
+        }
+          */
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+
+                    if (isEditing)
+                    {
+                        Console.WriteLine(toDoList.Rows[toDoListView.CurrentCell.RowIndex]["id"].GetType());
+                        // Update existing entry
+                        long id = (long)toDoList.Rows[toDoListView.CurrentCell.RowIndex]["id"];
+                        
+
+                        string updateQuery = "UPDATE ToDoItems SET Title=@title, Description=@desc WHERE id=@id";
+                        using (var command = new SQLiteCommand(updateQuery, connection))
+                        {
+                            command.Parameters.AddWithValue("@id", id);
+                            command.Parameters.AddWithValue("@title", titleTextBox.Text);
+                            command.Parameters.AddWithValue("@desc", descTextBox.Text);
+                            command.ExecuteNonQuery();
+                        }
+
+                        // Update data table row
+                        toDoList.Rows[toDoListView.CurrentCell.RowIndex]["Title"] = titleTextBox.Text;
+                        toDoList.Rows[toDoListView.CurrentCell.RowIndex]["Description"] = descTextBox.Text;
+
+                        toDoList.AcceptChanges();
+                    }
+                    else
+                    {
+                        // New entry
+                        string insertQuery = "INSERT INTO ToDoItems (Title, Description) VALUES (@title, @desc)";
+                        using (var command = new SQLiteCommand(insertQuery, connection))
+                        {
+                            command.Parameters.AddWithValue("@title", titleTextBox.Text);
+                            command.Parameters.AddWithValue("@desc", descTextBox.Text);
+                            command.ExecuteNonQuery();
+
+                            // Retrieve the new ID:
+
+                            // SELECT last_insert_rowid() 
+                            string identityQuery = "SELECT last_insert_rowid()";
+
+                            using (var command2 = new SQLiteCommand(identityQuery, connection))
+                            {
+                                long newId = (long)command2.ExecuteScalar();
+                                if (!isEditing)
+                                {
+                                    // New entry
+                                    toDoList.Rows.Add(newId, titleTextBox.Text, descTextBox.Text);
+                                    toDoList.AcceptChanges();
+                                }
+
+                            }
+
+
+
+                        }
+                    }
+
+                    // Clear temp fields after change
+                    titleTextBox.Text = "";
+                    descTextBox.Text = "";
+                    isEditing = false;
+
+                    ToDoList_Load(sender, e);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error saving data: " + ex.Message);
+                // Add user-friendly error notification in your application
+            }
+        }
+
+    }
+
+}
